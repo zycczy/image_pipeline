3eee33e1b7234bdcc90e18488f01f18a49159b52
Zhaoyuan Cheng <zycczyby@gmail.com>
2025-05-21 14:59:37 +0800

    optimize the code
    Signed-off-by: Zhaoyuan Cheng <zycczyby@gmail.com>

--- image_proc/include/image_proc/hal/opencv/rectify_opencv.hpp
+++ image_proc/include/image_proc/hal/opencv/rectify_opencv.hpp
@@ -25,29 +25,6 @@ public:
 
   void initialize(rclcpp::Node * node, int interpolation) override;
 
-  image_transport::Publisher createPublisher(
-    rclcpp::Node * node,
-    const std::string & topic,
-    const rmw_qos_profile_t & qos_profile,
-    const rclcpp::PublisherOptions & options);
-
-  image_transport::Subscriber createImageSubscription(
-    rclcpp::Node * node,
-    const std::string & topic,
-    const std::function<void(
-      const sensor_msgs::msg::Image::ConstSharedPtr &)> & callback,
-    const std::string & transport,
-    const rmw_qos_profile_t & qos_profile);
-
-  image_transport::CameraSubscriber createCameraSubscription(
-    rclcpp::Node * node,
-    const std::string & topic,
-    const std::function<void(
-      const sensor_msgs::msg::Image::ConstSharedPtr &,
-      const sensor_msgs::msg::CameraInfo::ConstSharedPtr &)> & callback,
-    const std::string & transport,
-    const rclcpp::QoS & qos);
-
   // Process image implementation (not an override)
   void processImage(
     rclcpp::Node * node,
@@ -56,36 +33,6 @@ public:
     image_geometry::PinholeCameraModel & model,
     const image_transport::Publisher & publisher,
     int interpolation);
-
-  // Template specialization for standard ROS Image
-  template<typename MsgT>
-  void imageCb(
-    rclcpp::Node * node,
-    const typename MsgT::ConstSharedPtr & image_msg,
-    const sensor_msgs::msg::CameraInfo::ConstSharedPtr & info_msg,
-    image_geometry::PinholeCameraModel & model,
-    int interpolation)
-  {
-    if (!image_msg || !info_msg) {
-      RCLCPP_ERROR(node->get_logger(), "Invalid image or camera info message");
-      return;
-    }
-
-    RCLCPP_DEBUG(node->get_logger(), "OpenCV HAL: Processing image");
-    
-    // Create publisher if needed
-    auto pub = image_transport::create_publisher(node, "image_rect");
-    
-    // Process the image
-    if constexpr (std::is_same_v<MsgT, sensor_msgs::msg::Image>) {
-      processImage(node, std::static_pointer_cast<const sensor_msgs::msg::Image>(image_msg), 
-                  info_msg, model, pub, interpolation);
-    } else {
-      RCLCPP_WARN(
-        node->get_logger(),
-        "Unhandled message type in OpenCV HAL imageCb - no processing performed");
-    }
-  }
 
   // 新增虚函数实现，统一入口
   void imageCb(
--- image_proc/src/hal/opencv/rectify_opencv.cpp
+++ image_proc/src/hal/opencv/rectify_opencv.cpp
@@ -25,45 +25,6 @@ void RectifyOpenCV::initialize(rclcpp::Node * node, int interpolation)
   RCLCPP_INFO(node->get_logger(), "Initialized RectifyOpenCV HAL with interpolation: %d", interpolation);
 }
 
-image_transport::Publisher RectifyOpenCV::createPublisher(
-  rclcpp::Node * node,
-  const std::string & topic,
-  const rmw_qos_profile_t & qos_profile,
-  const rclcpp::PublisherOptions & options)
-{
-  // Create standard image_transport publisher
-  (void)options;
-  RCLCPP_DEBUG(node->get_logger(), "Creating standard image publisher for %s", topic.c_str());
-  return image_transport::create_publisher(node, topic, qos_profile);
-}
-
-image_transport::Subscriber RectifyOpenCV::createImageSubscription(
-  rclcpp::Node * node,
-  const std::string & topic,
-  const std::function<void(
-    const sensor_msgs::msg::Image::ConstSharedPtr &)> & callback,
-  const std::string & transport,
-  const rmw_qos_profile_t & qos_profile)
-{
-  // Create standard image subscription using image_transport
-  RCLCPP_DEBUG(node->get_logger(), "Creating standard image subscription for %s", topic.c_str());
-  return image_transport::create_subscription(node, topic, callback, transport, qos_profile);
-}
-
-image_transport::CameraSubscriber RectifyOpenCV::createCameraSubscription(
-  rclcpp::Node * node,
-  const std::string & topic,
-  const std::function<void(
-    const sensor_msgs::msg::Image::ConstSharedPtr &,
-    const sensor_msgs::msg::CameraInfo::ConstSharedPtr &)> & callback,
-  const std::string & transport,
-  const rclcpp::QoS & qos)
-{
-  // Create standard camera subscription using image_transport
-  RCLCPP_DEBUG(node->get_logger(), "Creating standard camera subscription for %s", topic.c_str());
-  return image_transport::create_camera_subscription(
-    node, topic, callback, transport, qos.get_rmw_qos_profile());
-}
 
 void RectifyOpenCV::processImage(
   rclcpp::Node * node,
